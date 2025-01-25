90bab7cec1f333505133b52e81b8417107dd7051
sourav <devbhsourav@gmail.com>
2025-01-25 23:16:58 +0530

    [sourav]add : folderStructure

---
+++ BookStoreApiJavatore/BookStoreApiJavatore/src/main/java/com/sourav/bookstore/BookStoreApiJavatoreApplication.java
@@ -0,0 +1,13 @@
+package com.sourav.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApiJavatoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApiJavatoreApplication.class, args);
+	}
+
+}
---
+++ BookStoreApiJavatore/BookStoreApiJavatore/src/test/java/com/sourav/bookstore/BookStoreApiJavatoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourav.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiJavatoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
